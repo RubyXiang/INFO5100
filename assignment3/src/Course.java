9384e549599a8a252156ffa461fa9f40ca684bce
RubyXiang <48229408+RubyXiang@users.noreply.github.com>
2020-10-02 20:44:37 +0800

    Add files via upload

---
+++ assignment3/src/Course.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private String name;
+    private int numberOfStudent = 0;
+    List<Student> students = new ArrayList<>();
+
+    public Course(String name){
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public boolean isFull(int numberOfStudent) {
+        if(numberOfStudent >= 10)
+            return true;
+        else
+            return false;
+    }
+
+    public int getNumberOfStudent() {
+        return numberOfStudent;
+    }
+
+    public void setNumberOfStudent(int numberOfStudent) {
+        this.numberOfStudent = numberOfStudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void registerStudent(Student student) {
+        if(numberOfStudent < 10){
+            students.add(student);
+            numberOfStudent++;
+            System.out.println("Successful!!!");
+        }else{
+            System.out.println("Fail!!!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", numberOfStudent=" + numberOfStudent +
+                '}';
+    }
+
+}
---
+++ assignment3/src/Test.java
@@ -0,0 +1,39 @@
+public class Test {
+    public static void main(String args[]){
+        Course course = new Course("info5100");
+        course.isFull(course.getNumberOfStudent());
+        Student student1 = new Student("Ruby",10001);
+        course.registerStudent(student1);
+
+        Student student2 = new Student("Lucy",10002);
+        course.registerStudent(student2);
+
+        Student student3 = new Student("Lily",10003);
+        course.registerStudent(student3);
+
+        Student student4 = new Student("Luc",10004);
+        course.registerStudent(student4);
+
+        Student student5 = new Student("Ann",10005);
+        course.registerStudent(student5);
+
+        Student student6 = new Student("Lee",10006);
+        course.registerStudent(student6);
+
+        Student student7 = new Student("Coco",10007);
+        course.registerStudent(student7);
+
+        Student student8 = new Student("Mary",10008);
+        course.registerStudent(student8);
+
+        Student student9 = new Student("Joyce",10009);
+        course.registerStudent(student9);
+
+        Student student10 = new Student("Hordor",100010);
+        course.registerStudent(student10);
+
+        Student student11 = new Student("Jayden",100011);
+        course.registerStudent(student11);
+
+    }
+}
